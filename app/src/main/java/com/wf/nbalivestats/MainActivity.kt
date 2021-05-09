b988e637c636b1d1b49bddce4ad48e8bda757964
witoldwp <witold.filarowski@gmail.com>
2021-05-09 11:27:37 +0200

    RecyclerView NBA_Teams rebuild #2

--- app/src/main/java/com/wf/nbalivestats/MainActivity.kt
+++ app/src/main/java/com/wf/nbalivestats/MainActivity.kt
@@ -1,13 +1,8 @@
 package com.wf.nbalivestats
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log.i
-import android.widget.Button
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.wf.nbalivestats.adapter.TeamNameAdapter
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
@@ -37,46 +32,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rV_teams = findViewById<RecyclerView>(R.id.rv_teams)
-
-
-        // Creating an instance of our NameAdapter class and setting it to our RecyclerView
-        val teamNameList = getListOfTeamNames()
-        val TeamNamesAdapter = TeamNameAdapter(teamNameList)
-        rV_teams.adapter = TeamNamesAdapter
-        // Setting our RecyclerView's layout manager equal to LinearLayoutManager
-        rV_teams.layoutManager = LinearLayoutManager(this)
-
-
-        //NewActivity launcher "ActivityNbaTeams"
-        val id_btn_teams = findViewById<Button>(R.id.id_btn_teams)
-        id_btn_teams.setOnClickListener {
-            val intent = Intent(this, ActivityNbaTeams::class.java)
-            // start your next activity
-            startActivity(intent)
-        }
-
-        //NewActivity launcher "ActivityNbaPlayers"
-        val id_btn_players = findViewById<Button>(R.id.id_btn_players)
-        id_btn_players.setOnClickListener {
-            val intent = Intent(this, ActivityNbaPlayers::class.java)
-            // start your next activity
-            startActivity(intent)
-        }
-
-        //NewActivity launcher "ActivityNbaGames"
-        val id_btn_games = findViewById<Button>(R.id.id_btn_games)
-        id_btn_games.setOnClickListener {
-            val intent = Intent(this, ActivityNbaGames::class.java)
-            // start your next activity
-            startActivity(intent)
-        }
-
         logging.setLevel(HttpLoggingInterceptor.Level.BODY)
-
-
-
-
 
         call.enqueue(object : Callback<NbaTeams> {
             override fun onResponse(call: Call<NbaTeams>, response: Response<NbaTeams>) {
@@ -92,19 +48,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
         )
-    }
-
-    // This function just creates a list of names for us
-    private fun getListOfTeamNames(): MutableList<String> {
-        val teamNameList = mutableListOf<String>()
-        teamNameList.add("Ali")
-        teamNameList.add("Sophia")
-        teamNameList.add("Isabella")
-        teamNameList.add("Mason")
-        teamNameList.add("Jacob")
-        teamNameList.add("William")
-        teamNameList.add("Olivia")
-        return teamNameList
     }
 }
 
--- app/src/main/java/com/wf/nbalivestats/adapter/TeamNameAdapter.kt
+++ app/src/main/java/com/wf/nbalivestats/adapter/TeamNameAdapter.kt
@@ -7,24 +7,19 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.wf.nbalivestats.R
 
-// info_team_id
-// We will extend this class from RecyclerView.Adapter class next step
 
 
 class TeamNameAdapter(private val TeamNames: List<String>) :
-    RecyclerView.Adapter<TeamNameAdapter.TeamNameViewHolder>(){
+    RecyclerView.Adapter<TeamNameAdapter.TeamNameViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TeamNameViewHolder {
-        // Inflating R.layout.name_item
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_team_details, parent, false)
         return TeamNameViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: TeamNameViewHolder, position: Int) {
-        // Getting element from names list at this position
         val element = TeamNames[position]
-        // Updating the text of the txtName with this element
         holder.info_team_id.text = element
     }
 
@@ -32,9 +27,7 @@ class TeamNameAdapter(private val TeamNames: List<String>) :
         return TeamNames.size
     }
 
-
     class TeamNameViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val info_team_id = itemView.findViewById(R.id.info_team_id) as TextView
     }
-
 }
